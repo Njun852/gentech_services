fb787885ee97f868c4e0fd7c6044332818a5c9ff
Lala Wong <lorjade.1323@gmail.com>
2025-11-13 22:22:16 +0800

    Reapply "Added icons and images in resources"
    This reverts commit d438511a304959d1e2e165231c611e22abcf0be9.

---
+++ Views/UserControls/IconButton.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gentech_services.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for IconButton.xaml
+    /// </summary>
+    public partial class IconButton : UserControl
+    {
+        
+        public IconButton()
+        {
+            InitializeComponent();
+            BorderContainer.MouseLeftButtonUp += (s, e) =>
+            {
+                RaiseEvent(new RoutedEventArgs(ClickEvent));
+            };
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(IconButton));
+
+        public ImageSource Icon
+        {
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(IconButton));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        // click event
+        public static readonly RoutedEvent ClickEvent =
+            EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(IconButton));
+
+        public event RoutedEventHandler Click
+        {
+            add => AddHandler(ClickEvent, value);
+            remove => RemoveHandler(ClickEvent, value);
+        }
+        private void BorderContainer_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(ClickEvent));
+        }
+    }
+}
